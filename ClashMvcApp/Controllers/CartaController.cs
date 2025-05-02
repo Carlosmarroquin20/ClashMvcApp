3dee546a231aeaee212a3537091df58c87e32a48
Emanuel Marroquin <carlosema322@gmail.com>
2025-05-02 12:22:43 -0600

    Add CartaController with static card list and Index view

---
+++ ClashMvcApp/Controllers/CartaController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace ClashMvcApp.Controllers
+{
+    public class CartaController : Controller
+    {
+        public IActionResult Index()
+        {
+            // Lista estática de ejemplo
+            var cartas = new List<string>
+            {
+                "Archers",
+                "Knight",
+                "Giant",
+                "Wizard",
+                "Tesla"
+            };
+            return View(cartas);
+        }
+    }
+}
