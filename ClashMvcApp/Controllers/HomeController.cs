92615e757971cd9ae02ec212e8da3d816626d490
Emanuel Marroquin <carlosema322@gmail.com>
2025-05-02 11:55:04 -0600

    Add project files.

---
+++ ClashMvcApp/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ClashMvcApp.Models;
+
+namespace ClashMvcApp.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    //Pagina principal de index o si no se pone /
+    public IActionResult Index()
+    {
+        ViewData["Mensaje"] = "Bienvenido al catalogo de Clash Royale";
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
